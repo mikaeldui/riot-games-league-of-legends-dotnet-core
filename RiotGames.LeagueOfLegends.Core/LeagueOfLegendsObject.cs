30ebe70f2e6eca96cb24b8048f30f0f2f354f0d9
Mikael Dúi Bolinder <github@bolinder.uk>
2022-02-17 11:22:14 +0100

    Update LeagueOfLegendsObject.cs

--- RiotGames.LeagueOfLegends.Core/LeagueOfLegendsObject.cs
+++ RiotGames.LeagueOfLegends.Core/LeagueOfLegendsObject.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace RiotGames.LeagueOfLegends
 {
+    /// <summary>
+    /// Provides a base class for League of Legends objects, excluding collections.
+    /// </summary>
     public abstract class LeagueOfLegendsObject : RiotGamesObject, ILeagueOfLegendsObject
     {
     }
